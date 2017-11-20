970bc486794c4739c995e43b2b77167a2e8a76c4
martin.skarbala <martin.skarbala@itera.no>
2017-11-20 12:00:28 +0100

    added login runner

---
+++ src/test/java/runners/LoginRunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber"},
+        features = {"src/test/resources/scenarios/Login.feature"},
+        glue = {"steps"}
+
+)
+
+public class LoginRunner {
+
+
+}
